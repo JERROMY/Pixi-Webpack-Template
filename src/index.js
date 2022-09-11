a6076f3ace92f18ac0a5ff07ff6097210488bba8
Jerromy <jerromy_lee@nox.com.tw>
2022-09-12 00:11:00 +0800

    init Project

---
+++ src/index.js
@@ -0,0 +1,18 @@
+
+import Container3D from "./components/container-3d/container-3d"
+import ThreeData from "./three-data"
+import ThreeMain from "./components/three-main/three-main"
+import './main.scss'
+
+const container3D = new Container3D()
+container3D.render()
+
+const threeData = new ThreeData()
+const threeMain = new ThreeMain(threeData)
+
+
+if (process.env.NODE_ENV === 'production') {
+    console.log('Production mode')
+}else{
+    console.log('Development mode')
+}
---
+++ webpack.dev.config.js
@@ -0,0 +1,113 @@
+
+const path = require( 'path' );
+// const TerserPlugin = require( 'terser-webpack-plugin' );
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+const { CleanWebpackPlugin } = require( 'clean-webpack-plugin' );
+const HtmlWebpackPlugin = require( 'html-webpack-plugin' )
+
+/*
+
+module
+asset/resource
+asset/inline
+asset/
+asset/source
+
+css-loader => MiniCssExtractPlugin.loader
+
+*/
+
+module.exports = {
+
+    entry: {
+        'index':'./src/index.js',
+        'three-data':'./src/three-data.js',
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve( __dirname, './dist' ),
+        publicPath: '',
+        //assetModuleFilename: 'images/[hash][ext][query]'
+    },
+    mode: 'development',
+    devServer: {
+        port: 9000,
+        static: {
+            directory: path.resolve( __dirname, './dist' ),
+        },
+        devMiddleware: {
+            index: 'index.html',
+            writeToDisk: true,
+        }
+    },
+    module: {
+        rules:[
+            {
+                test: /\.(png|jpg)$/,
+                type: 'asset',
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 3 * 1024,
+                    }
+                }
+            },
+            {
+                test:/\.txt/,
+                type: 'asset/source'
+            },
+            {
+                test: /\.(glsl|vs|fs|vert|frag)$/i,
+                type: 'asset/source',
+                // generator:
+                // {
+                //     filename: 'assets/shaders/[hash][ext]'
+                // }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader, 'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [ '@babel/env' ],
+                        plugins: [ '@babel/plugin-proposal-class-properties' ]
+                    }
+                }
+            },
+            {
+                test: /\.hbs$/,
+                use: [
+                    'handlebars-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        // new TerserPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+        }),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            title: 'Three.js 3D Web Template',
+            // template: 'src/page-template.hbs',
+            description: 'Test 3D Template',
+            minify: false,
+        }),
+    ]
+
+
+};
